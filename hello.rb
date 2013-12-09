abeb1e90f3ec09252a8c58c4450d855a037d7544
JamesGraham <jamesgraham09@gmail.com>
2013-12-09 16:02:37 +0000

    basic kitten test site

---
+++ hello.rb
@@ -0,0 +1,25 @@
+require 'sinatra'
+
+get '/' do
+	@name = %w(Srip Evgeny Kiko Moggler).sample
+	erb :index	
+end
+
+get '/hello' do
+	@visitor = params[:name]
+	erb :index
+
+end
+
+get '/secret' do
+	'This is a secret page'
+end
+
+get '/coffee' do
+	'How about some golden brown'
+end
+
+get '/kitty' do
+	'hello kitty'
+end
+
